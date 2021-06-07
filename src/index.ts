3c8293fa76ab0032689392e158f984f9223d55fb
Paul Bottein <paul.bottein@gmail.com>
2021-06-07 12:27:19 +0200

    fix: use 0.0.0.0 address for server

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const PORT = Number(process.env.PORT) || 9207;
 const app = fastify();
 
 app.register(controller);
-app.listen(PORT);
+app.listen(PORT, '0.0.0.0');
 
 console.log(`🚀 YAS 207 bridge running on port ${PORT}`);
